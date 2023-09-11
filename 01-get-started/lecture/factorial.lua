a770be5cb2b4830eb637885b92dc2a4c437cc846
Daniel Vong <dvwl@hotmail.com>
2023-09-11 11:14:56 +0800

    Update factorial.lua
    Fix poorly written tabs.

--- 01-get-started/lecture/factorial.lua
+++ 01-get-started/lecture/factorial.lua
@@ -1,12 +1,12 @@
 -- defines a factorial function
 function fact (n)
 	if n == 0 then
-	return 1
+		return 1;
 	else
-	return n * fact(n - 1)
+		return n * fact(n - 1);
 	end
-	end
+end
 	
-	print("enter a number:")
-	a = io.read("*n") -- reads a number
-	print(fact(a))
+print("enter a number:");
+a = io.read("*n"); -- reads a number
+print(fact(a));
