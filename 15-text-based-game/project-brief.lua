0592bb5c4a64683216247b98204eb0b90e118c1e
Daniel Vong <dvwl@hotmail.com>
2023-09-17 22:28:31 +0800

    15-text-based-game
    A simple text-based game project.

---
+++ 15-text-based-game/project-brief.lua
@@ -0,0 +1,27 @@
+-- You are to create a text-based game in the terminal that incorporates various Lua concepts you've learned. Here's an example of a simple text-based adventure game.
+
+-- In this game, players navigate through Lua Land, facing challenges and making choices. The game will utilize the following Lua concepts:
+
+-- 1. Data Types: Use Lua's data types to represent player attributes, items, and game state.
+
+-- 2. Functions: Implement functions for game logic, player actions, and interactions.
+
+-- 3. I/O: Utilise io.write and io.read for displaying text and receiving player input. You may even store game data and configuration to and from a text file.
+
+-- 3. Closures: Store game data and functions within closures for encapsulation.
+
+-- 4. Pattern Matching: Use pattern matching to handle player input and match choices.
+
+-- 5. Metatables and Metamethods: Employ metatables to define custom behaviors for in-game objects.
+
+-- 6. Object-Oriented Programming: Create classes or objects for characters, items, and locations.
+
+-- 7. Garbage Collection: Manage memory usage and cleanup using Lua's automatic garbage collection.
+
+-- 8. Coroutines: Implement coroutines for game events or time-based challenges.
+
+-- How you define Lua Land, what's in Lua Land, who's in Lua Land, the actions that you can do in Lua Land, it's all up to you.
+
+-- Again, you can work in pairs for this.
+
+-- Good luck in Lua Land!
