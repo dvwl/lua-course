2569debf80c457b1d6df2fd3a1f92e6a3f22b2c2
Daniel Vong <dvwl@hotmail.com>
2023-09-19 23:37:34 +0800

    16-first-project-on-roblox-studio

---
+++ 16-first-project-on-roblox-studio/exercise/exercise01.lua
@@ -0,0 +1,7 @@
+-- In Roblox Studio, experiment with the creation tools by creating a mini obstacle course. It could be individual parts the player has to avoid, or it could be a lava floor.
+
+-- Tips:
+-- Use a single large part and insert a script as you did earlier to turn it into lava.
+-- Additional models can be found in the Toolbox; just be aware that some models may already have scripts in them.
+-- Don’t forget to anchor all parts and models.
+-- If you know how to use the terrain tools, you can work that into your obstacle course as well
