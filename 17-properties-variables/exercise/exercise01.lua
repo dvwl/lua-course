b64422cb3e26629e8524d69be80117bd1441a6fc
Daniel Vong <dvwl@hotmail.com>
2023-09-19 23:49:54 +0800

    17-properties-variables

---
+++ 17-properties-variables/exercise/exercise01.lua
@@ -0,0 +1,5 @@
+-- Continue from 16-first-project-on-roblox-studio, make a NPC for your maze. 
+-- For this exercise, see if you can create the NPC from start to finish only using code. 
+
+-- Tips:
+-- Use Instance.new() to create the part that will act as the NPC's body, the Dialog object.
