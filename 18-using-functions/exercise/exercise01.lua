833675e99bcde16f6d2b5281e5dc226e80cef354
Daniel Vong <dvwl@hotmail.com>
2023-09-20 21:11:02 +0800

    18-using-functions

---
+++ 18-using-functions/exercise/exercise01.lua
@@ -0,0 +1,7 @@
+-- You may choose to continue from 16-first-project-on-roblox-studio.
+-- Make bridge or track piece that solidfies after a player touches a button and then resets in your maze. 
+
+-- Tips:
+-- It’ll be easier to use a single bridge part rather than multiple parts.
+-- Place the script in the button to take advantage of the Touched event.
+-- Use wait() to control how long the bridge is active.
