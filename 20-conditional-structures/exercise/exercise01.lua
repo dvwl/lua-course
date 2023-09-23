f221576892d3e5fc4f6cc4f9b408ff8ad7869f40
Daniel Vong <dvwl@hotmail.com>
2023-09-23 16:34:57 +0800

    Update exercise01.lua
    Fix typos

--- 20-conditional-structures/exercise/exercise01.lua
+++ 20-conditional-structures/exercise/exercise01.lua
@@ -1,4 +1,4 @@
--- Give aplayer super powers by making them go fast when they touch a speed booster! A Humanoid’s default WalkSpeed property is set to 16. Not so bad, but it’d be a lot cooler to go a lot faster. Make a part that temporarily allows a user to go way faster and then returns them to their original speed after a few seconds. 
+-- Give a player super powers by making them go fast when they touch a speed booster! A Humanoid’s default WalkSpeed property is set to 16. Not so bad, but it’d be a lot cooler to go a lot faster. Make a part that temporarily allows a user to go way faster and then returns them to their original speed after a few seconds. 
 
 -- To do so, you can use the onTouch pattern you’ve been working with and some if/then statements. As an added twist, use a ParticleEmitter object to stream sparkles behind them while they’re powered up.
 
