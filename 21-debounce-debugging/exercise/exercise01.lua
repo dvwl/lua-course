5e0ea71078d61015059dc99e466b74d384dd83a9
Daniel Vong <dvwl@hotmail.com>
2023-09-24 12:04:01 +0800

    21-debounce-debugging

---
+++ 21-debounce-debugging/exercise/exercise01.lua
@@ -0,0 +1,4 @@
+-- A large part of any engineer’s job is thinking about both what can go wrong and how to make things  better. Think about the code you’ve created in all of the hours up until now, and for this first exercise, write down at least three ways in which the code could be better. It might be things that can go wrong with the code, or features that would enable people to better enjoy your experience.
+
+-- You might not be able to code the solutions just yet, but you should get in the habit of being critical of the code you’re creating.
+
---
+++ 21-debounce-debugging/exercise/exercise02.lua
@@ -0,0 +1,12 @@
+-- Make two pickups: one that makes you smaller and one that makes you larger. Instead of setting three specific sizes, use a multiplier to change the avatar’s current scale.
+
+-- Remember to create a debounce variable to control how fast the person is allowed to grow and shrink. This is one case where if the function is triggered more often than intended, your experience will crash.
+
+-- Tips:
+-- You can modify the user’s default scale with the following properties: 
+-- Humanoid.HeadScale: Scale of the avatar’s head.
+-- Humanoid.BodyDepthScale: Scale of the body’s depth.
+-- Humanoid.BodyWidthScale: Scale of the body’s width.
+-- Humanoid.BodyHeightScale: Scale of the body’s height.
+-- Set up a simple debounce before experimenting with the avatar’s scale to avoid crashes.
+-- Save your work before testing! If the avatar’s scale gets too big, it will crash your experience.
