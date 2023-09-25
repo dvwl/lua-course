0e56801f49ae3e4b928f7fbd965fb2a83b893498
Daniel Vong <dvwl@hotmail.com>
2023-09-25 14:19:51 +0800

    22-loops

---
+++ 22-loops/exercise/exercise01.lua
@@ -0,0 +1,1 @@
+-- Modify the code seen in the example so that players have to collect wood for the fire rather than being able to simply walk up to a fire and light it.
---
+++ 22-loops/exercise/exercise02.lua
@@ -0,0 +1,3 @@
+-- The conceptof Damage Over Time (DoT) is used in lots of experiences. With DoT, people take ongoing damage for a certain amount of time rather than taking it all at once. Common examples include encountering poison or taking burn damage after touching a fire.
+
+-- Because you already have a fire from an earlier sections, for this exercise, use the same model to temporarily inflict burn damage to anyone who happens to touch it.
