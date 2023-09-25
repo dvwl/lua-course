e993606ce4a6422b1c526d218ab5b37a8ba87361
Daniel Vong <dvwl@hotmail.com>
2023-09-25 14:22:24 +0800

    23-arrays

---
+++ 23-arrays/exercise/exercise01.lua
@@ -0,0 +1,5 @@
+-- One way developers make their experiences feel more tied to the real world is by updating assets as seasons go by. In this exercise, see if you can figure out how to make the pine tree in go from a summery green to a wintery white.
+
+-- Tips:
+-- In the ToolBox, search the pine tree with endorsed model logo.
+-- Assume there’s a whole forest of trees in your game, you're going to need more than one loop.
