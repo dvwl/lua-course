07353c2879c7c300b6b8fefc389711363bb25873
Daniel Vong <dvwl@hotmail.com>
2023-09-26 15:57:17 +0800

    24-dictionaries

---
+++ 24-dictionaries/exercise/exercise01.lua
@@ -0,0 +1,9 @@
+-- Earlier in this section, a person was assigned to the "Red" team upon joining the experience. 
+
+-- For this exercise, can you figure out how to alternate team assignments between "Red" and "Blue"? 
+
+-- Print the members of each team.
+
+-- Tips:
+-- Test using Network Simulator
+-- Instances can't concatenate with strings, but the name of the instance will.
