638d14f74d3d5e0bb9673bd2787714e9a33ea70f
Daniel Vong <dvwl@hotmail.com>
2023-09-26 16:32:22 +0800

    25-client-server

---
+++ 25-client-server/exercise/exercise01.lua
@@ -0,0 +1,3 @@
+-- In this section, one problemwith the current shop is that the price of the items aren’t listed. However, remember that only one function can be bound to a RemoteFunction at a time. 
+
+-- Create a second RemoteFunction and use it to display not only the name of items people can purchase, but also the cost.
