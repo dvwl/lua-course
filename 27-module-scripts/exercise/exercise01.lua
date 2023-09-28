5f726f017f3961b85311cdad10dd0a70a2dc7f69
Daniel Vong <dvwl@hotmail.com>
2023-09-28 09:17:49 +0800

    27-module-scripts

---
+++ 27-module-scripts/exercise/exercise01.lua
@@ -0,0 +1,3 @@
+-- In this section, practice creating a module script by using code you're familiar with.
+
+-- Make a series of trap parts people neeed to avoid if they don't want to lose all their health.
