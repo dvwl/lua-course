fd99cedd32023b03ead5aa3716a359220f3e97a1
Daniel Vong <dvwl@hotmail.com>
2023-09-28 09:15:36 +0800

    26-remote-events

---
+++ 26-remote-events/exercise/exercise01.lua
@@ -0,0 +1,7 @@
+-- In this section, recall the example on the map choice.
+
+-- Once the map is picked, be sure to announce the map choice to everyone in the server.
+
+-- Tips:
+-- Remember that clients can't talk directly to other clients.
+-- You don't need another RemoteEvent.
