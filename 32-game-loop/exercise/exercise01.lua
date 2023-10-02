df0e0b6e672ecfe170f354450577fccd089954a8
Daniel Vong <dvwl@hotmail.com>
2023-10-02 18:45:33 +0800

    32-game-loop

---
+++ 32-game-loop/exercise/exercise01.lua
@@ -0,0 +1,7 @@
+-- It’s always good to keep players informed of what’s about to happen to them. Create a new ModuleScript designed to announce the beginning and end of every match. For now, it’s fine to just practice with BindableEvents and simply print “Round Starting” and “Round Over” at the beginning and end of each round. In an actual experience, you would want to set up the UI to make the announcement to the players.
+
+-- Tips
+-- Create a separate module named Announcements.
+-- Within the module, create separate functions for the announcements at the beginning and end of the match.
+-- Add the module to those required in RoundManager.
+-- Use print statements to check everything, and then if you want to, move to creating more finalized UI code.
