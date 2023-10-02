97e7f74a9c7f9eec547eae7d3e98ebcc805266ef
Daniel Vong <dvwl@hotmail.com>
2023-10-02 18:43:34 +0800

    31-saving-data

---
+++ 31-saving-data/exercise/exercise01.lua
@@ -0,0 +1,6 @@
+-- Using the information in this section, you should be able to award people five pieces of gold every time they log in. You can display people’s gold on a leaderboard, but for the sake of this exercise, you’re only being asked to print out how much gold a person has after it’s been updated.
+-- This could be something like a daily login reward.
+
+-- Tips
+-- Make sure you use the player’s ID rather than their name.
+-- Don’t forget to check success while reading and updating the Data Store.
